db838038170532df7edbae6ddd42a95dab990396
SickanK <sixtenklementsson@gmail.com>
2023-06-13 17:11:21 +0200

    autoformatter and added back tab for navigation in lsp quick menu

--- macos/nvim/after/plugin/lsp.lua
+++ macos/nvim/after/plugin/lsp.lua
@@ -14,14 +14,9 @@ lsp.nvim_workspace()
 local cmp = require('cmp')
 local cmp_select = {behavior = cmp.SelectBehavior.Select}
 local cmp_mappings = lsp.defaults.cmp_mappings({
-  ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-  ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
   ['<C-m>'] = cmp.mapping.confirm({ select = true }),
   ["<C-Space>"] = cmp.mapping.complete(),
 })
-
-cmp_mappings['<Tab>'] = nil
-cmp_mappings['<S-Tab>'] = nil
 
 lsp.setup_nvim_cmp({
   mapping = cmp_mappings
@@ -37,6 +32,7 @@ lsp.set_preferences({
     }
 })
 
+
 lsp.on_attach(function(_, bufnr)
   local opts = {buffer = bufnr, remap = false}
 
@@ -50,6 +46,8 @@ lsp.on_attach(function(_, bufnr)
   vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
   vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
   vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+
+  lsp.buffer_autoformat()
 end)
 
 lsp.setup()
